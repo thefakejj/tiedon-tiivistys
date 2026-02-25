01e1520217b9b52ae48ced346a83189e6f8e9f1a
thefakejj <jasper.laurila@gmail.com>
2026-02-25 18:39:56 +0200

    fixed whitespace bug

--- src/main.py
+++ src/main.py
@@ -6,9 +6,13 @@ def get_text_from_file(path):
     with open(path, encoding="ASCII") as f:
     #with open(path) as f: <- if I allow utf-8
         text = f.read()
-        if text[-1] == "\n":
-            return text[:-1]
-        return text
+        # if text[-1] == "\n":
+        #     last_i = -1
+        #     while text[last_i-1] == "\n":
+        #         last_i -= 1 #reduce by 1 until line breaks are stripped
+        #     return text[:last_i]
+        # return text
+        return text.strip() # removes leading and trailing whitespace. might have to implement the algorithms without this though.
 
 def compare_file_size(og_path, bin_path):
     og_size = os.path.getsize(og_path)
@@ -18,6 +22,7 @@ def compare_file_size(og_path, bin_path):
 if __name__ in "__main__":
 
     og_path = "./src/sampletexts/johndoe.txt"
+    # og_path = "./src/tests/test_texts/16MB.txt"
     filetext = get_text_from_file(og_path)
     print("file has been read\n")
     bin_path = "./binfile.bin"
@@ -27,13 +32,14 @@ if __name__ in "__main__":
     encode_lz(filetext, bin_path)
     compare_file_size(og_path, bin_path)
     lz_output = decode_lz(bin_path)
+    #print(lz_output)
     print(f"text matches: {filetext==lz_output}")
-    print(lz_output)
 
     # HUFFMAN
     print("saving with huffman\n")
     encode_huffman(filetext, bin_path)
     compare_file_size(og_path, bin_path)
     huffman_output = decode_huffman(bin_path)
+    #print(huffman_output)
     print(f"text matches: {filetext==huffman_output}")
-    print(huffman_output)
+    
